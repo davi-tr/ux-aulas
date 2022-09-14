3cc785ccabfa0e8304be5d49bffc8a696bcc0936
davi-tr <davitrajano473@gmail.com>
2022-09-14 03:20:10 -0300

    Modo escuro ativado

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
-const button = document.getElementById('btn');
-
-function valor(){
-  console.log("Worked")
+function changeMode(){
+  changeClass();
+  changeBtn();
+  changeImg()
 }
 
-button.addEventListener('click', valor);
+function changeClass(){
+    body.classList.toggle(darkModeClass);
+}
+
+function changeBtn(){
+  button.classList.toggle(darkModeClass);
+}
+
+const icon = document.getElementById('btn');
+let toggle = true;
+
+function changeImg(){
+    if (image.src == "https://img.icons8.com/ios-glyphs/90/000000/github.png"){
+      image.src = "https://img.icons8.com/ios-glyphs/90/FFFFFF/github.png";
+    } else{
+      image.src = "https://img.icons8.com/ios-glyphs/90/000000/github.png"
+    }
+}
+
+const darkModeClass = 'dark-mode'
+const body = document.getElementsByTagName('body')[0];
+
+
+const button = document.getElementById('btn');
+const image = document.getElementById('git');
+
+
+button.addEventListener('click', changeMode);
